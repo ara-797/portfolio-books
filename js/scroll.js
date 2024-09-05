1eb38b8caa6a7ce395dca1a4c1f4db86b32ab950
ara-797 <wknowgo797@gmail.com>
2024-09-05 14:43:24 +0900

    메인 페이지 - Gallery 영역 추가 및 섹션 스크롤 이벤트 구현

---
+++ js/scroll.js
@@ -0,0 +1,45 @@
+// Scroll Event - Throttling
+let eventBlocker = null;
+let speed = 100;
+
+window.addEventListener('scroll', () => {
+	if (eventBlocker) return;
+
+	eventBlocker = setTimeout(() => {
+		activation();
+		eventBlocker = null;
+	}, speed);
+});
+
+// Scroll Navigation
+const btns = document.querySelectorAll('.scroll-navi li');
+
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', () => {
+		window.scrollTo({
+			top: sections[idx].offsetTop,
+			behavior: 'smooth',
+		});
+	});
+});
+
+// 메인 페이지 Section Scroll
+const sections = document.querySelectorAll('.my-scroll');
+const baseline = -200;
+
+function activation() {
+	const scroll = window.scrollY;
+
+	if (scroll <= 500) {
+		for (const el of sections) el.classList.remove('on');
+	}
+
+	sections.forEach((_, idx) => {
+		if (scroll >= sections[idx].offsetTop + baseline) {
+			for (const btn of btns) btn.classList.remove('on');
+			btns[idx].classList.add('on');
+
+			sections[idx].classList.add('on');
+		}
+	});
+}
